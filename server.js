23ff31b922c878356995488d5bce9123bca25c68
vaishnavichilaka24-maker <vaishnavi.chilaka24@sasi.ac.in>
2026-07-14 13:09:43 +0530

    Add DNS server configuration to server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const noteRoutes = require("./routes/noteRoutes");
-// const dns= require("dns")
-// dns.setServers(["8.8.8.8","8.8.4.4"])
+const dns= require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 
 dotenv.config();
 
